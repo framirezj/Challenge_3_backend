74bf7609f1ad00e136350e9fbae978e3f5857697
framirezj <12649259+framirezj@users.noreply.github.com>
2024-07-11 21:49:02 -0400

    Cargo los archivos del proyecto

---
+++ src/main/java/one/oracle/ch_3_back/Ch3BackApplication.java
@@ -0,0 +1,13 @@
+package one.oracle.ch_3_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch3BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/oracle/ch_3_back/Ch3BackApplicationTests.java
@@ -0,0 +1,13 @@
+package one.oracle.ch_3_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
