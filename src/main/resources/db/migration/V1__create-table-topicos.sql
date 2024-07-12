423027565354e91c7e2f7d20859073b12596a2d6
framirezj <12649259+framirezj@users.noreply.github.com>
2024-07-12 19:36:45 -0400

    create: tabla topicos

--- src/main/resources/db/migration/V1__create-table-topicos.sql
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -1,15 +1,12 @@
 create table topicos(
-                        id bigint not null auto_increment,
-                        nombre varchar(100) not null,
-                        email varchar(100) not null,
-                        documento varchar(10) not null unique,
-                        especialidad varchar(100) not null,
-                        calle varchar(100) not null,
-                        distrito varchar(100) not null,
-                        complemento varchar(100),
-                        numero int(20),
-                        ciudad varchar(100) not null,
-                        status tinyint default true,
 
-                        primary key(id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    mensaje TEXT NOT NULL,
+    fechaCreacion DATETIME NOT NULL,
+    activo TINYINT(1) NOT NULL DEFAULT 1,
+    autor VARCHAR(255) NOT NULL,
+    curso VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id)
 );
