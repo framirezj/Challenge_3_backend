11e1f8e7060a3391c05eb895eb908985263d9365
framirezj <12649259+framirezj@users.noreply.github.com>
2024-07-12 20:31:50 -0400

    create: topico repository

---
+++ src/main/java/one/oracle/ch_3_back/domain/topico/TopicoRepository.java
@@ -0,0 +1,9 @@
+package one.oracle.ch_3_back.domain.topico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    
+}
